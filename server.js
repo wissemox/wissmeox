d6275265ccf3be7ff764572ebd8227ab81ff59b3
wissem <wissemox02@gmail.com>
2021-07-08 14:06:37 +0200

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const ConnectDb = require('./config/connectDb')
 const auth = require('./routes/auth')
 const Admin = require('./routes/Admin')
 const Product =require('./routes/Product')
+const path = require('path')
 const app = express()
 app.use(express.json())
 //Conncte data base 
@@ -12,13 +13,16 @@ app.use('/api/auth',auth)
 app.use('/api/Admin',Admin)
 app.use('/api/Product',Product)
 //Server
-const PORT = process.env.PORT || 5000 
+if(process.env.NODE_ENV==='production') {
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+const port = process.env.PORT || 5000 
 
-app.listen(PORT , (error)=>{
+app.listen(port , (error)=>{
     error ? 
     console.log(error)
-    :console.log('the server is runnig on port' ,PORT)
+    :console.log('the server is runnig on port' ,port)
 })
-if(process.env.NODE_ENV==="production") {
-    app.use(express.static('client/build'))
-}
