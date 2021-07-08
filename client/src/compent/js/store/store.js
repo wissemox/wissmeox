674e1e3e5ed12f3ccaddb8d919af53369c8981c0
wissem <wissemox02@gmail.com>
2021-07-08 16:58:16 +0200

    update

--- client/src/compent/js/store/store.js
+++ client/src/compent/js/store/store.js
@@ -6,7 +6,6 @@ const store = createStore(
     rootReducer, 
     compose(
         applyMiddleware(...middlerwares),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 ); 
 export default store
